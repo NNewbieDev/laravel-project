9b91797a80128485d1896bca4c906d82feb77970
2051010215 <93323654+2051010215@users.noreply.github.com>
2023-10-04 17:28:45 +0700

    2.1.1

--- front-end/src/pages/ArticleDetail.jsx
+++ front-end/src/pages/ArticleDetail.jsx
@@ -35,7 +35,7 @@ const ArticleDetail = () => {
       let { data } = await authApi().post(endpoints['comments'](articleId), {
         "content": content,
       });
-      setComments([data, ...comments]);
+      setComments([data.data, ...comments]);
     }
     process();
   }
@@ -84,31 +84,32 @@ const ArticleDetail = () => {
               {moment(article.updated_at).utc().format('HH:mm DD-MM-YYYY')}
             </span>
           </div>
-          
-          <h1 className="ms-16 text-2xl">Thảo Luận: </h1>
-          {user === null ? <p className=" pb-5 my-5 ms-20 text-lg">Vui lòng <Link to={url}>đăng nhập</Link> để bình luận!</p>
-            : <><div className="flex items-center justify-center shadow-lg mt-10 mx-8 mb-4 max-w-full">
-              <form className="w-full max-w-full bg-white rounded-lg px-4 pt-2">
-                <div className="flex flex-wrap -mx-3 mb-6">
-                  <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">Thêm Bình Luận Mới:</h2>
-                  <div className="w-full md:w-full px-3 mb-2 mt-2">
-                    <textarea className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-14 py-2 px-2 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder="Cảm nghĩ của bạn..." required defaultValue={""} />
-                  </div>
-                  <div className="w-full flex items-start md:w-full px-3">
-                    <div className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
-                      <svg fill="none" className="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                      </svg>
-                      <p className="text-xs md:text-sm pt-px">Some HTML is okay.</p>
+
+          <h1 className="mt-10 ms-20 text-2xl">Thảo Luận: </h1>
+          {user === null ? <p className=" pb-5 mb-5 ms-20 text-xl">Vui lòng <Link className="text-sky-500" to={url}>đăng nhập</Link> để bình luận!</p>
+            : <>
+              <div className="flex items-center justify-center shadow-lg mt-3 mx-8 mb-4 max-w-full">
+                <form className="w-full max-w-full bg-white rounded-lg px-4 pt-2">
+                  <div className="flex flex-wrap -mx-3 mb-6">
+                    <h2 className="px-4 pt-3 pb-2 text-gray-800 text-xl">Thêm bình luận mới:</h2>
+                    <div className="w-full md:w-full px-3 mb-2 mt-2">
+                      <input className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full text-base h-16 py-2 px-2 placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder="Cảm nghĩ của bạn..." required defaultValue={""} />
                     </div>
-                    <div className="-mr-1">
-                      <Button type="submit" onClick={addComment} className="bg-white text-gray-700 font-normal py-1 px-4 border border-gray-400 rounded-lg mr-1 hover:bg-gray-100" defaultValue="Post Comment">
-                        Thêm Ý kiến
-                      </Button>
+                    <div className="w-full flex items-start md:w-full px-3">
+                      <div className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                        <svg fill="none" className="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        
+                      </div>
+                      <div className="-mr-1">
+                        <Button type="submit" onClick={addComment} className="bg-white text-gray-700 font-normal py-1 px-4 border border-gray-400 rounded-lg mr-1 hover:bg-gray-100" defaultValue="Post Comment">
+                          Thêm Ý kiến
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </div></form>
-            </div>
+                  </div></form>
+              </div>
             </>}
 
 
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -161,7 +161,7 @@ const Home = () => {
               type="text"
               onChange={(e) => setKw(e.target.value)}
               value={kw}
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block w-full p-4 pl-10 text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Tìm kiếm..."
               required
             />
