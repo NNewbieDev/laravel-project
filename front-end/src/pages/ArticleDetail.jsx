4b0b87f5a05fe3df3bfddda68491cdda06682edf
2051010215 <93323654+2051010215@users.noreply.github.com>
2023-10-15 22:24:16 +0700

    final_1.0

--- front-end/src/pages/ArticleDetail.jsx
+++ front-end/src/pages/ArticleDetail.jsx
@@ -76,7 +76,7 @@ const ArticleDetail = () => {
       //     behavior: "smooth",
       //   });
       // }
-      setToastContent(response.data)
+      setToastContent(response.data);
       let { data } = await Apis.get(endpoints["getRatings"](articleId));
       setRating(data);
     };
@@ -154,6 +154,151 @@ const ArticleDetail = () => {
                 </svg>
                 {moment(article.updated_at).utc().format("HH:mm DD-MM-YYYY")}
               </span>
+
+              {/* Phần Rating */}
+              <div className="flex w-4/6 lg:ms-28 justify-end relative z-50 ">
+                <form onSubmit={(e) => addRating(e)}>
+                  <div className="flex items-center flex-row-reverse">
+                    <button
+                      className="transition duration-300 w-4 h-4 text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 mr-1"
+                      type="submit"
+                    >
+                      <svg
+                        onClick={(event) => setRating(event.currentTarget.id)}
+                        id="5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    </button>
+                    <button
+                      className="transition duration-300 w-4 h-4 text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 mr-1"
+                      type="submit"
+                    >
+                      <svg
+                        onClick={(event) => setRating(event.currentTarget.id)}
+                        id="4"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    </button>
+                    <button
+                      className="transition duration-300 w-4 h-4 text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 mr-1"
+                      type="submit"
+                    >
+                      <svg
+                        onClick={(event) => setRating(event.currentTarget.id)}
+                        id="3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    </button>
+                    <button
+                      className="transition duration-300 w-4 h-4 text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 mr-1"
+                      type="submit"
+                    >
+                      <svg
+                        onClick={(event) => setRating(event.currentTarget.id)}
+                        id="2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    </button>
+                    <button
+                      className="transition duration-300 w-4 h-4 text-gray-600 cursor-pointer peer peer-hover:text-yellow-400 hover:text-yellow-400 mr-1"
+                      type="submit"
+                    >
+                      <svg
+                        onClick={(event) => setRating(event.currentTarget.id)}
+                        id="1"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    </button>
+
+                    {/*  */}
+                    {rating !== "" ? (
+                      <span className=" me-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+                        {rating} out of 5
+                      </span>
+                    ) : (
+                      <span className=" me-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+                        0 out of 5
+                      </span>
+                    )}
+                  </div>
+                </form>
+              </div>
+
+              {/*Phần report bài viết*/}
+              <form
+                onSubmit={(e) => addReport(e)}
+                className="group cursor-pointer transition duration-300 border-2 rounded-md p-3 relative -mt-3 ml-3 hover:bg-neutral-100 me-10"
+              >
+                <FlagIcon
+                  className="mx-3 hover:text-blue-400 group-hover:text-blue-400"
+                  width={24}
+                />
+                <div className="group-hover:flex sm:rounded-lg flex-col rounded-lg sm:min-w-[300px] p-3 drop-shadow-xl sm:absolute hidden bg-white sm:top-10 sm:right-0">
+                  <button
+                    type="submit"
+                    onClick={(event) =>
+                      setReport(event.currentTarget.textContent)
+                    }
+                    className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 p-2 hover:bg-neutral-200 rounded-lg"
+                  >
+                    <div className="">
+                      <FontAwesomeIcon icon={faPlusCircle} />
+                    </div>
+                    <div className="">
+                      Bài viết chứa nội dung không phù hợp!
+                    </div>
+                  </button>
+                  <button
+                    type="submit"
+                    onClick={(event) =>
+                      setReport(event.currentTarget.textContent)
+                    }
+                    className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 p-2 hover:bg-neutral-200 rounded-lg"
+                  >
+                    <div className="">
+                      <FontAwesomeIcon icon={faPlusCircle} />
+                    </div>
+                    <div className="">Bài viết dễ gây kích động!</div>
+                  </button>
+                  <button
+                    type="submit"
+                    onClick={(event) =>
+                      setReport(event.currentTarget.textContent)
+                    }
+                    className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 p-2 hover:bg-neutral-200 rounded-lg"
+                  >
+                    <div className="">
+                      <FontAwesomeIcon icon={faPlusCircle} />
+                    </div>
+                    <div className="">Bài viết có nội dung ko phù hợp!</div>
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
 
@@ -269,71 +414,51 @@ const ArticleDetail = () => {
                       </form>
                     </div>
 
-              {/*Phần report bài viết*/}
-              <form
-                onSubmit={(e) => addReport(e)}
-                className="group cursor-pointer transition duration-300 border-2 rounded-md p-3 relative -mt-3 ml-3 hover:bg-neutral-100 me-10"
-              >
-                <FlagIcon className="mx-3" color="Red" width={24} />
-                <div className="group-hover:flex sm:rounded-lg flex-col rounded-lg sm:min-w-[300px] p-3 drop-shadow-xl sm:absolute hidden bg-white sm:top-10 sm:right-0">
-                  <button
-                    type="submit"
-                    onClick={(event) =>
-                      setReport(event.currentTarget.textContent)
-                    }
-                    className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 px-2 py-2 hover:bg-neutral-200 rounded-lg"
-                  >
-                    <div className="">
-                      <FontAwesomeIcon icon={faPlusCircle} />
-                    </div>
-                    <div className="">Bài viết có chứa yếu tố phản động!</div>
-                  </button>
-                  <button
-                    type="submit"
-                    onClick={(event) =>
-                      setReport(event.currentTarget.textContent)
-                    }
-                    className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 px-2 py-2 hover:bg-neutral-200 rounded-lg"
-                  >
-                    <div className="">
-                      <FontAwesomeIcon icon={faPlusCircle} />
-                    </div>
-                    <div className="">Bài viết có chứa ngôn ngữ đả kích!</div>
-                  </button>
-                  <button
-                    type="submit"
-                    onClick={(event) =>
-                      setReport(event.currentTarget.textContent)
-                    }
-                    className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 px-2 py-2 hover:bg-neutral-200 rounded-lg"
-                  >
-                    <div className="">
-                      <FontAwesomeIcon icon={faPlusCircle} />
-                    </div>
-                    <div className="">Bài viết có nội dung ko phù hợp!</div>
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          {/* Phần comment */}
-          {user === null ? (
-            <p className=" my-5 ms-5 lg:ms-20 text-xl">
-              Vui lòng{" "}
-              <Link className="text-sky-500" to={url}>
-                đăng nhập
-              </Link>{" "}
-              để bình luận!
-            </p>
-          ) : (
-            <>
-              <div className="flex items-center justify-center shadow-lg mt-3 mx-8 mb-4 max-w-full">
-                <form
-                  className="w-full max-w-full bg-white rounded-lg px-4 pt-2"
-                  onSubmit={(e) => addComment(e)}
-                >
-                  <div className="flex flex-wrap -mx-3 mb-6">
+                    {/*Phần report bài viết*/}
+                    <form
+                      onSubmit={(e) => addReport(e)}
+                      className="group cursor-pointer transition duration-300 border-2 rounded-md p-3 relative -mt-3 ml-3 hover:bg-neutral-100 me-10"
+                    >
+                      <FlagIcon className="mx-3" color="Red" width={24} />
+                      <div className="group-hover:flex sm:rounded-lg flex-col rounded-lg sm:min-w-[300px] p-3 drop-shadow-xl sm:absolute hidden bg-white sm:top-10 sm:right-0">
+                        <button
+                          type="submit"
+                          onClick={(event) =>
+                            setReport(event.currentTarget.textContent)
+                          }
+                          className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 px-2 py-2 hover:bg-neutral-200 rounded-lg"
+                        >
+                          <div className="">
+                            <FontAwesomeIcon icon={faPlusCircle} />
+                          </div>
+                          <div className="">Bài viết có chứa yếu tố phản động!</div>
+                        </button>
+                        <button
+                          type="submit"
+                          onClick={(event) =>
+                            setReport(event.currentTarget.textContent)
+                          }
+                          className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 px-2 py-2 hover:bg-neutral-200 rounded-lg"
+                        >
+                          <div className="">
+                            <FontAwesomeIcon icon={faPlusCircle} />
+                          </div>
+                          <div className="">Bài viết có chứa ngôn ngữ đả kích!</div>
+                        </button>
+                        <button
+                          type="submit"
+                          onClick={(event) =>
+                            setReport(event.currentTarget.textContent)
+                          }
+                          className="flex gap-2 hover:cursor-pointer min-w-[15rem] transition duration-500 px-2 py-2 hover:bg-neutral-200 rounded-lg"
+                        >
+                          <div className="">
+                            <FontAwesomeIcon icon={faPlusCircle} />
+                          </div>
+                          <div className="">Bài viết có nội dung ko phù hợp!</div>
+                        </button>
+                      </div>
+                    </form>
                     <h2 className="px-4 pt-3 pb-2 text-gray-800 text-xl">
                       Thêm bình luận:
                     </h2>
